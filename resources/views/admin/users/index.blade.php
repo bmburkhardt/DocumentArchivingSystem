d67f218681ee4e35bd0fcc930d731a10e5232738
bmburkhardt <bmburkhardt@alaska.edu>
2018-04-26 22:26:26 -0500

    User update implemented

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -12,6 +12,7 @@
         <thead>
             <tr>
                 <th>Id</th>
+                <th>Photo</th>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Role</th>
@@ -26,7 +27,8 @@
             @foreach($users as $user)
                 <tr>
                     <td>{{$user->id}}</td>
-                    <td>{{$user->name}}</td>
+                    <td><img><img height="50" src="{{$user->photo ? $user->photo->path : '/images/default.jpg'}}" alt=""></td>
+                    <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</td>
                     <td>{{$user->email}}</td>
                     <td>{{$user->role ? $user->role->name : 'User has no role'}}</td>
                     <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
